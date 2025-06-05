a36dcbfa3f3e58ffd28caba9ae631cc44d953169
Luca <lglucagngr@gmail.com>
2025-06-05 16:55:14 +0200

    Fix PostgreSQL SSL pour Render

--- server/config/database.js
+++ server/config/database.js
@@ -1,15 +1,25 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const isPostgres = process.env.DB_DIALECT === 'postgres';
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || 'mysql', // par défaut MySQL, mais Render pourra utiliser postgres
-    port: process.env.DB_PORT || 3306, // par défaut MySQL, mais Render utilisera 5432
-    logging: false
+    port: process.env.DB_PORT || (isPostgres ? 5432 : 3306),
+    dialect: process.env.DB_DIALECT || 'mysql',
+    logging: false,
+    dialectOptions: isPostgres
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
   }
 );
 
